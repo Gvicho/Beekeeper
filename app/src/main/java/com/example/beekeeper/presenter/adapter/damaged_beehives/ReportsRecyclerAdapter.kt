54f8db16d4ac5d7919fff453de0839b8f517ca82
GiorgiGvichiani <gio_gvichiani@mail.ru>
2024-03-18 11:18:43 +0400

    damage report page modified, recycler changed with pager

--- app/src/main/java/com/example/beekeeper/presenter/adapter/damaged_beehives/ReportsRecyclerAdapter.kt
+++ app/src/main/java/com/example/beekeeper/presenter/adapter/damaged_beehives/ReportsRecyclerAdapter.kt
@@ -3,11 +3,14 @@ package com.example.beekeeper.presenter.adapter.damaged_beehives
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.example.beekeeper.R
 import com.example.beekeeper.databinding.ReportRecyclerItemBinding
+import com.example.beekeeper.presenter.adapter.ImagesPager2Adapter
 import com.example.beekeeper.presenter.model.damaged_beehives.DamageReportUI
+import me.relex.circleindicator.CircleIndicator3
 
 class ReportsRecyclerAdapter() :
     ListAdapter<DamageReportUI, ReportsRecyclerAdapter.ReportsViewHolder>(ReportsDiffUtil()) {
@@ -21,40 +24,51 @@ class ReportsRecyclerAdapter() :
     )
 
     override fun onBindViewHolder(holder: ReportsViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(position)
     }
 
     inner class ReportsViewHolder(private val binding: ReportRecyclerItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        private lateinit var model: DamageReportUI
+
+        private lateinit var imagesPager2Adapter: ImagesPager2Adapter
+        private lateinit var viewPager2: ViewPager2
+        private lateinit var indicator: CircleIndicator3
 
 
-        fun bind() {
-            model = currentList[adapterPosition]
-            val picturesList = model.imageUris
-            picturesList.let {
-                val picturesAdapter = ReportPicturesRecyclerAdapter(it.map { uri-> uri.toString() })
-                binding.picturesRecyclerView.adapter = picturesAdapter
-                
-            }
+        fun bind(pos:Int) {
+            val report = currentList[pos]
+            val picturesList = report.imageUris.map { it.toString() }
+            val damage = report.damageLevelIndicator
+
+            imagesPager2Adapter = ImagesPager2Adapter(picturesList)
+
+            viewPager2 = binding.viewPager2
+            indicator = binding.indicator
+
+            viewPager2.adapter = imagesPager2Adapter
+            indicator.setViewPager(viewPager2)
+            imagesPager2Adapter.registerAdapterDataObserver(indicator.adapterDataObserver)
+
+
             binding.apply {
-
-                tvDate.text = model.dateUploaded
-                tvDesc.text = model.damageDescription
-                tvId.text = model.id.toString()
-                picturesRecyclerView.layoutManager = GridLayoutManager(root.context, 3)
-
-
-
+                tvDate.text = report.dateUploaded
+                tvDesc.text = report.damageDescription
+                tvId.text = report.id.toString()
+                if(damage>2){
+                    indicatorLogo.setImageResource(R.drawable.ic_red_cross_28)
+                }else{
+                    indicatorLogo.setImageResource(R.drawable.ic_warning_yellow_28)
+                }
             }
-            listeners()
+
+           // listeners()
         }
 
-        private fun listeners() {
-            binding.root.setOnClickListener {
-
-            }
-        }
+//        private fun listeners() {
+//            binding.root.setOnClickListener {
+//
+//            }
+//        }
 
     }
 
