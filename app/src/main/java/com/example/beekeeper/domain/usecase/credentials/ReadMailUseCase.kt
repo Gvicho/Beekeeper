a71b98e3b183da39ae28162a87ca2210b90502a7
GiorgiGvichiani <gio_gvichiani@mail.ru>
2024-03-20 19:48:49 +0400

    read mail from datastore added

---
+++ app/src/main/java/com/example/beekeeper/domain/usecase/credentials/ReadMailUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.beekeeper.domain.usecase.credentials
+
+import com.example.beekeeper.domain.repository.data_store.DataStoreRepository
+import com.example.beekeeper.domain.utils.PreferencesKeys
+import javax.inject.Inject
+
+class ReadMailUseCase@Inject constructor(private val dataStoreRepository: DataStoreRepository) {
+
+    operator fun invoke() = dataStoreRepository.readString(PreferencesKeys.MAIL)
+
+}
